81a58a1979c70bc7fa871eaba2f5827185a7e9c4
jackson <jackson.dobson2026@gbwl.org>
2024-10-15 13:03:25 +0000

    x

--- Main.java
+++ Main.java
@@ -2,5 +2,26 @@ public class Main {
 
    public static void main(String []args) {
       System.out.println("Hello World");
+
+      int age = 15;
+      int year = 14; 
+
+      boolean match = (age == year);
+
+      System.out.println(match);
+
+      year = 15;
+      System.out.println( age == year);
+
+      System.out.println( age != year);
+
+      double pi = 3.141592653859;
+      System.out.println(pi > 0); 
+      System.out.println(pi < 0);
+      System.out.println(pi >= 3.14);
+      System.out.println(pi <= 10.00);
+      
+
+
    }
 }
