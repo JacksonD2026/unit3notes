bf516bb779cbaf35deb9982d70a7d4f6e42784a7
jackson <jackson.dobson2026@gbwl.org>
2024-10-16 13:37:50 +0000

    bryce is a dummy for forgetting his cleats

--- Main.java
+++ Main.java
@@ -22,6 +22,46 @@ public class Main {
       System.out.println(pi <= 10.00);
       
 
+      // if statements
+
+      boolean isCold = false;
+
+      if (isCold == true) {
+         System.out.println("Bring a jacket!");
+      }
+
+      if (isCold == false) {
+         System.out.println("Enjoy the weather");
+      }
+
+      // magic eight ball
+      int rando = (int) (Math.random() * 8) + 1;
+
+      if (rando == 1) {
+         System.out.println( "No chance.");
+      }
+      if (rando == 2) {
+         System.out.println( "Meh.");
+      }
+      if (rando == 3) {
+         System.out.println( "Nah.");
+      }
+      if (rando == 4) {
+         System.out.println( "Most definitely.");
+      }
+      if (rando == 5) {
+         System.out.println( "Sources say yes.");
+      }
+      if (rando == 6) {
+         System.out.println( "Perchance.");
+      }
+      if (rando == 7) {
+         System.out.println( "Absolutely!");
+      }
+      if (rando == 8) {
+         System.out.println( "Absolutely no chance.");
+      }
+
 
    }
 }
